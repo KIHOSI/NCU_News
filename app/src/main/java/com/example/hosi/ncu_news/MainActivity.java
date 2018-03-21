cb9de3036322b98825d5b37b764ca057cc74bd91
KIHOSI <amie1234@gmail.com>
2018-03-21 16:04:01 +0800

    add strings.xml(zh:taiwan & eg-rUS)

--- app/src/main/java/com/example/hosi/ncu_news/MainActivity.java
+++ app/src/main/java/com/example/hosi/ncu_news/MainActivity.java
@@ -23,14 +23,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);  右下角紅色的懸浮按鈕(app_bar_main.xml)
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(

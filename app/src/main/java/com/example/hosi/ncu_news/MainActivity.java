655cf50bc92dcbe75005ce7db8c1381753244404
KIHOSI <amie1234@gmail.com>
2018-03-25 17:50:54 +0800

    let innercontent has navigation drawer

--- app/src/main/java/com/example/hosi/ncu_news/MainActivity.java
+++ app/src/main/java/com/example/hosi/ncu_news/MainActivity.java
@@ -146,12 +146,12 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {  右上角點選設定
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
